8134867b142ac95dd74533aae510e617eca12c74
FalkSturmfels <fritzachim2@gmail.com>
2016-11-19 00:08:22 +0100

    Changed brand in nav bar

--- resources/views/partials/mainnav.blade.php
+++ resources/views/partials/mainnav.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href={{url('/')}}>NebulaMundi</a>
+            <a class="navbar-brand" href={{url('/')}}>Nebula Mundi</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
