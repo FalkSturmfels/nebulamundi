510547480b264784e2d237f6f9e3cd990972a238
FalkSturmfels <fritzachim2@gmail.com>
2016-11-19 23:36:31 +0100

    Specifiy properties in Adventure and World model, which could used for mass instantiation.

--- app/Adventure.php
+++ app/Adventure.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Adventure extends Model
 {
+    protected $fillable = ['name', 'description', 'status'];
+
     public function world()
     {
         return $this->belongsTo(World::class);
--- app/World.php
+++ app/World.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class World extends Model
 {
 
+    protected $fillable = ['name', 'description'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
